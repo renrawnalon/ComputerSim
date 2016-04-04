02fa61fb75c4b12797069f14ff8b7ab3baa336aa
Nolan Warner <renrawnalon@gmail.com>
2016-04-04 21:49:07 +0900

    Add Command tuple

---
+++ app/src/main/java/com/renrawnalon/computersim/computer/InvalidArgumentsException.java
@@ -0,0 +1,9 @@
+package com.renrawnalon.computersim.computer;
+
+public class InvalidArgumentsException extends Exception {
+
+    public InvalidArgumentsException(String message){
+        super(message);
+    }
+
+}
